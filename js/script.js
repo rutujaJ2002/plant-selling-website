90734865b667570d0d8cc483019a93aebe4e68cd
Rutuja Sanjay Jadhav <82172257+rutujaJ2002@users.noreply.github.com>
2022-04-27 18:31:51 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+let menu= document.querySelector('#menu-btn');
+let navbar=  document.querySelector('.navbar');
+let header=  document.querySelector('header-3');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if(window.scrollY >250){
+        header.classList.add('active');
+    }
+    else{
+        header.classList.remove('active');
+    }
+}
+
+let countDate = new Date('June 4, 2022 00:00:00').getTime();
+
+function countDown(){
+
+        let now = new Date().getTime();
+    
+        gap = countDate - now;
+    
+        let second = 1000;
+        let minute = second * 60;
+        let hour = minute * 60;
+        let day = hour * 24;
+    
+        let d = Math.floor(gap / (day));
+        let h = Math.floor((gap % (day)) / (hour));
+        let m = Math.floor((gap % (hour)) / (minute));
+        let s = Math.floor((gap % (minute)) / (second));
+    
+        document.getElementById('day').innerText = d;
+        document.getElementById('hour').innerText = h;
+        document.getElementById('minute').innerText = m;
+        document.getElementById('second').innerText = s;
+    
+}
+
+setInterval(function(){
+    countDown();
+},1000)
